76bdb0aa724909705614475f3285faa7cd10b3a7
John <dbltoe@users.noreply.github.com>
2026-02-06 14:03:34 -0600

    Fix formatting in lang.ot_extraorderfee.php

--- includes/languages/english/modules/order_total/lang.ot_extraorderfee.php
+++ includes/languages/english/modules/order_total/lang.ot_extraorderfee.php
@@ -4,10 +4,8 @@
  *
  * Array-based format required for Zen Cart 2.2.0+
  */
-
 $define = [
-    'MODULE_ORDER_TOTAL_EXTRAORDERFEE_TITLE'       => 'Extra Order Fee',
+    'MODULE_ORDER_TOTAL_EXTRAORDERFEE_TITLE' => 'Extra Order Fee',
     'MODULE_ORDER_TOTAL_EXTRAORDERFEE_DESCRIPTION' => 'Adds an extra fee to orders based on manufacturers, categories, or products',
 ];
-
 return $define;
