6467131a277855c4e65a53ba231185282eb27bbc
John <dbltoe@users.noreply.github.com>
2026-02-06 13:27:20 -0600

    Refactor Extra Order Fee language definitions
    Updated language definitions for Extra Order Fee module to use define syntax.

--- includes/languages/english/modules/order_total/lang.ot_extraorderfee.php
+++ includes/languages/english/modules/order_total/lang.ot_extraorderfee.php
@@ -1,14 +1,7 @@
 <?php
 /**
- * @copyright Copyright 2003-2024 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: dbltoe 2026 Jan 16 New in v2.2.0-alpha $
-*/
+ * Language defines for Extra Order Fee module
+ */
 
-$define = [
-    'MODULE_ORDER_TOTAL_EXTRAORDERFEE_TITLE' => 'Extra Order Fee',
-	'MODULE_ORDER_TOTAL_EXTRAORDERFEE_DESCRIPTION' => 'Extra Order Fee',
-	'MODULE_ORDER_TOTAL_EXTRAORDERFEE_SORT_ORDER' => '450'
-];
-
-return $define;
+define('MODULE_ORDER_TOTAL_EXTRAORDERFEE_TITLE', 'Extra Order Fee');
+define('MODULE_ORDER_TOTAL_EXTRAORDERFEE_DESCRIPTION', 'Adds an extra fee to orders based on manufacturers, categories, or products');
